20f01fd1a31c4d970396caa23de4fb3a7a74a93a
Mariam Jebran <mariamjebran@Mariams-MacBook-Pro.local>
2022-02-01 10:51:43 -0500

    add divisibleby3

--- introduction_to_python/functions/1_below_100/solution/solution.py
+++ introduction_to_python/functions/1_below_100/solution/solution.py
@@ -1,4 +1,1 @@
 # Write your solution here
-def concat_args(*args):
-    print(args)
-concat_args(3, 4, 5, 'ok')
--- introduction_to_python/functions/3_divisible_by_3/solution/solution.py
+++ introduction_to_python/functions/3_divisible_by_3/solution/solution.py
@@ -1,1 +1,12 @@
 # Write your code here
+from re import A
+
+
+def divisible_by_3(*args):
+    final_list = []
+    for number in args:
+        if number % 3 == 0:
+            final_list.append(number)
+    return final_list
+
+divisible_by_3(2 , 3, 5, 6, 9)
